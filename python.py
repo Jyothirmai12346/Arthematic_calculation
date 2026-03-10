1917dc472cde5580a6bf6366ebb20b1a41b3a653
Jyothirmai12346 <1972csraju@gmail.com>
2026-03-10 12:38:44 +0530

    Create python.py

---
+++ python.py
@@ -0,0 +1,21 @@
+import sys
+
+num1 = int(sys.argv[1])
+num2 = int(sys.argv[2])
+operation = sys.argv[3]
+
+result = 
+if operation == "add":
+    result = num1 + num2
+elif operation == "sub":
+    result = num1 - num2
+elif operation == "mul":
+    result = num1 * num2
+else:
+    print("Invalid operation")
+    sys.exit()
+
+print("Number1:", num1)
+print("Number2:", num2)
+print("Operation:", operation)
+print("Result:", result)
